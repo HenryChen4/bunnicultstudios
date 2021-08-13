13121b511309ffc77dca73863d7f47e353959696
Henry Chen <benryben@b2o2.attlocal.net>
2021-08-13 17:07:19 -0500

    initial front and backend

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const hbs = require("hbs")
+const app = express()
+
+const port = process.env.port || 8080
+
+app.set("view engine", "hbs")
+app.set("views", "")
+
+app.get("", (req, res)=>{
+    res.render("index")
+})
+
+app.listen(port, ()=>{
+    console.log("Listening from server at port: "+port);
+})
