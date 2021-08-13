60ce767c8cf11ab05b6117a60c8c07dab898596f
Henry Chen <benryben@b2o2.attlocal.net>
2021-08-13 17:16:35 -0500

    fixed a port issue??

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const hbs = require("hbs")
 const app = express()
 
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 
 app.set("view engine", "hbs")
 app.set("views", "")
